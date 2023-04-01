275f70e5e54a7968902417b8656fa0eb4e83a084
Apostolia Maria Sofianopoulou <81187477+Sofianopoulou@users.noreply.github.com>
2023-04-01 15:02:32 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
 			File outputFile = new File("output.txt");
 			FileWriter writer = new FileWriter(outputFile);
